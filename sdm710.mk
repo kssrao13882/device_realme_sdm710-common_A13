5ac6f3617595e0c9201ccc7ff5203e60fecb5ade
someone5678 <nemui3353@gmail.com>
2022-10-04 19:39:15 +0530

    sdm710-common: Enable google LILY_EXPERIENCE feature
    * Extracted from Google Camera Go official release 3.6.455515391
    * Checked from Google Camera Go, Gallery Go
    * It enables several Android Go specific features for Android Go Apps
    
    Example:
    Google Camera Go:
    * HDR, Night Mode, Snapchat Sticker integration
    
    Change-Id: I93cc1070bec2e6b7c31440f06232255913592c6c
    Signed-off-by: someone5678 <nemui3353@gmail.com>
    Signed-off-by: Cykeek <Cykeek@proton.me>
    Signed-off-by: kssrao13882 <kssrao13882@gmail.com>

--- sdm710.mk
+++ sdm710.mk
@@ -265,6 +265,10 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl.recovery \
     android.hardware.health@2.1-service
 
+# Google LILY_EXPERIENCE feature
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/lily_experience.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/lily_experience.xml
+
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
